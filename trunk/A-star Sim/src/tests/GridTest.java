58b4d4bb16e84c6a9b09aea8ae2c3f8625c9f133
Lironkatav <Lironkatav@0d38ea09-b483-8cac-562d-f7f06ffec48e>
2010-11-13 18:08:20 +0000

    adding test for the grid

---
+++ trunk/A-star Sim/src/tests/GridTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+import GUI.Cell;
+import GUI.Grid;
+import GUI.Cell.Status;
+import Simulation.mainFrame;
+
+
+public class GridTest {
+	
+	public static void main(String args[]) {
+		mainFrame main = new mainFrame("A* Simulator");
+		Cell[][] grid = main.get_grid().get_grid();
+		grid[1][2].set_status(Status.Blocked);
+		grid[2][2].set_status(Status.Blocked);
+		grid[0][0].set_status(Status.Start);
+		grid[10][10].set_status(Status.Finish);
+		
+		
+		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		main.getContentPane().add(main.__mainPanel, BorderLayout.CENTER);
+		main.setSize(700, 550);
+		main.setResizable(false);
+		main.setVisible(true);
+	}
+
+}
