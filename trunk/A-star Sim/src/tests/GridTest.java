a2cf7b17948d849f44b8c14a2f5d66354338bd05
Lironkatav <Lironkatav@0d38ea09-b483-8cac-562d-f7f06ffec48e>
2010-11-13 23:12:33 +0000

    fixing the GUI, adding controller interface

--- trunk/A-star Sim/src/tests/GridTest.java
+++ trunk/A-star Sim/src/tests/GridTest.java
@@ -4,24 +4,13 @@ import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
 
-import GUI.Cell;
-import GUI.Cell.Status;
 import Simulation.mainFrame;
 
 
 public class GridTest {
 	
 	public static void main(String args[]) {
-		mainFrame main = new mainFrame("A* Simulator");
-		Cell[][] grid = main.get_grid().get_grid();
-		grid[1][2].set_status(Status.Blocked);
-		grid[2][2].set_status(Status.Blocked);
-		grid[3][7].set_status(Status.Start);
-		grid[3][8].set_status(Status.inOpenList);
-		grid[0][8].set_status(Status.inClosedList);
-		grid[10][10].set_status(Status.Finish);
-		
-		
+		mainFrame main = new mainFrame("A* Simulator");		
 		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		main.getContentPane().add(main.__mainPanel, BorderLayout.CENTER);
 		main.setSize(700, 550);
