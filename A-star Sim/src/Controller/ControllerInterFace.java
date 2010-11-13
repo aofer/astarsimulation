810b77c0ae354fe4476e83533016fa3c3d30d021
Lironkatav <Lironkatav@0d38ea09-b483-8cac-562d-f7f06ffec48e>
2010-11-13 23:14:09 +0000

    adding the controller interface and a stub class that implements it

---
+++ A-star Sim/src/Controller/ControllerInterFace.java
@@ -0,0 +1,29 @@
+package Controller;
+
+import java.util.Vector;
+
+import algorithms.myPoint;
+
+public interface ControllerInterFace {
+	
+	/**
+	 * returns the final Path
+	 * @param _startPoints
+	 * @param _endPoints
+	 * @param _blocked
+	 * @return
+	 */
+	public Vector<myPoint> findPath(myPoint[] startPoints,
+									myPoint[] endPoints, 
+									Vector<myPoint> _blocked);
+	/**
+	 * @return the openList
+	 */
+	public Vector<myPoint> getOpenList();
+	
+	/**
+	 * @return the closed list 
+	 */
+	public Vector<myPoint> getClosedList();
+
+}
