babd54db06105d41b38dd71ade010dcd13ff21ad
Lironkatav <Lironkatav@0d38ea09-b483-8cac-562d-f7f06ffec48e>
2010-11-20 15:43:08 +0000

    fixed alot of stuff

--- A-star Sim/src/Controller/Controller.java
+++ A-star Sim/src/Controller/Controller.java
@@ -3,6 +3,8 @@ package Controller;
 import heuristics.HeuristicInterface;
 import heuristics.ManhattanHeuristic;
 
+import java.util.Observable;
+import java.util.Observer;
 import java.util.Vector;
 
 import test.moverStub;
@@ -11,65 +13,112 @@ import maps.Mover;
 import maps.TileBasedMap;
 import maps.TileStatus;
 import maps.TiledMapImpl;
+import Events.ApplicationEvent;
+import Events.ApplicationEventListener;
+import Events.ApplicationEventListenerCollection;
+import Events.ApplicationEventSource;
+import Events.ClosedListChangeEvent;
+import Events.OpenListChangeEvent;
+import Events.finalPathEvent;
 import Simulation.mainFrame;
+import algorithms.AStarPathFinder;
+import algorithms.State;
 import algorithms.myPoint;
 
-public class Controller implements ControllerInterFace {
-	//private AStarPathFinder _pathFinder;
+public class Controller implements ControllerInterFace,ApplicationEventSource{
+	private AStarPathFinder _pathFinder;
 	private TileBasedMap _map;
 	private HeuristicInterface _heuristic;
 	private mainFrame _mainFrame;
-	private AStarPathFinderWorker _findPathWorker ;
-	
-	public Controller(mainFrame main){
-		this._map = new TiledMapImpl(10, 10, false);
+	private ApplicationEventListenerCollection _listeners;
+	private pathFinderThread _thread;
+	public Controller(mainFrame main) {
+		this._map = new TiledMapImpl(20, 20, false);
 		this._heuristic = new ManhattanHeuristic();
+		this._pathFinder = new AStarPathFinder(this._map, this._heuristic);
 		this._mainFrame = main;
-		//this._pathFinder = new AStarPathFinder(this._map, this._heuristic);
-		
+		this._pathFinder.addListener(new ControllerListener());
+		this._listeners = new ApplicationEventListenerCollection();
+		this._thread = null;
 	}
-	
-	public Vector<Vector<myPoint>> findPath(myPoint[] startPoints, myPoint[] endPoints,
+
+	public void findPath(myPoint[] startPoints, myPoint[] endPoints,
 			Vector<myPoint> _blocked) {
-	//	AStarPathFinder pathfinder = new AStarPathFinder(this._map, new ManhattanHeuristic());
-		Vector<myPoint> starts = arrayToVector(startPoints);
-		Vector<myPoint> ends = arrayToVector(endPoints);
-		Vector<Mover> movers = new Vector<Mover>();
-		for (int i = 0; i < startPoints.length;i++){
+		final Vector<myPoint> starts = arrayToVector(startPoints);
+		final Vector<myPoint> ends = arrayToVector(endPoints);
+		final Vector<Mover> movers = new Vector<Mover>();
+		for (int i = 0; i < startPoints.length; i++) {
 			Mover m = new moverStub();
 			movers.add(m);
 		}
-		
-		this.set_findPathWorker(new AStarPathFinderWorker(this._mainFrame,starts, ends, movers, _map, _heuristic));
-		try {
-			this.get_findPathWorker().execute();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;	
+		this._thread = new pathFinderThread(starts,ends,movers);
+		this._thread.start();
+
 	}
 
-	
-	public void setTile(Vector<myPoint> blockedTiles){
-		for (myPoint p: blockedTiles){
+	public void setTile(Vector<myPoint> blockedTiles) {
+		for (myPoint p : blockedTiles) {
 			this._map.setTile(p.getX(), p.getY(), TileStatus.blocked);
 		}
 	}
-	public Vector<myPoint> arrayToVector(myPoint[] arr){
+
+	public Vector<myPoint> arrayToVector(myPoint[] arr) {
 		Vector<myPoint> res = new Vector<myPoint>();
-		for (int i = 0; i < arr.length;i++){
+		for (int i = 0; i < arr.length; i++) {
 			res.add(arr[i]);
 		}
 		return res;
 	}
 
-	public void set_findPathWorker(AStarPathFinderWorker _findPathWorker) {
-		this._findPathWorker = _findPathWorker;
+	protected class ControllerListener implements ApplicationEventListener {
+
+		@Override
+		public void handle(ApplicationEvent event) {
+			if (event instanceof finalPathEvent) {
+				Controller.this._listeners.fireEvent(event);
+			}
+			else if (event instanceof OpenListChangeEvent){
+				Controller.this._mainFrame.get_grid().setOpenListCell(((OpenListChangeEvent)event).get_points());
+			}
+			else if(event instanceof ClosedListChangeEvent){
+				Controller.this._mainFrame.get_grid().setClosedListCell(((ClosedListChangeEvent)event).get_points());
+			}
+
+		}
+
 	}
 
-	public AStarPathFinderWorker get_findPathWorker() {
-		return _findPathWorker;
+	@Override
+	public void addListener(ApplicationEventListener listener) {
+		this._listeners.add(listener);
+		
 	}
 
-	
+	@Override
+	public void clearListeners() {
+		this._listeners.clear();
+		
+	}
+
+	@Override
+	public void removeListener(ApplicationEventListener listener) {
+		this._listeners.remove(listener);
+		
+	}
+	public pathFinderThread getThread(){
+		return this._thread;
+	}
+	public class pathFinderThread extends Thread{
+		private Vector<myPoint> _starts;
+		private Vector<myPoint> _ends;
+		private Vector<Mover> _movers;
+		public pathFinderThread(Vector<myPoint> starts,Vector<myPoint> ends,Vector<Mover> movers){
+			this._starts = starts;
+			this._ends = ends;
+			this._movers = movers;
+		}
+		public void run(){
+			Controller.this._pathFinder.findPath(this._movers, this._starts, this._ends);
+		}
+	}
 }
--- A-star Sim/src/Simulation/Simulator.java
+++ A-star Sim/src/Simulation/Simulator.java
@@ -1,5 +1,7 @@
 package Simulation;
 
+import algorithms.AStarPathFinder;
+import algorithms.PathFinderInterface;
 import heuristics.HeuristicInterface;
 import heuristics.ManhattanHeuristic;
 import maps.TileBasedMap;
@@ -7,14 +9,13 @@ import maps.TiledMapImpl;
 
 public class Simulator {
 	
-	//private PathFinderInterface _pathFinder;
+	private PathFinderInterface _pathFinder;
 	private TileBasedMap _map;
 	private HeuristicInterface _heuristic;
 	
 	public Simulator(){
 		this._map = new TiledMapImpl(20, 20, false);
 		this._heuristic = new ManhattanHeuristic();
-		//this._pathFinder = new AStarPathFinder(this._map, this._heuristic);
-		
+		this._pathFinder = new AStarPathFinder(this._map, this._heuristic);		
 	}
 }
